ea387771a8a866bc0bd76054744ffc9fa3e9b794
nagi0101 <demon0315@naver.com>
2023-02-14 07:51:42 +0900

    11726

---
+++ dynamic-programming/11726.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX_N = 1002;
+
+int N;
+int dp[MAX_N] { 0, };
+
+
+int main()
+{
+    cin >> N;
+
+    dp[1] = 1;
+    dp[2] = 2;
+    for(int n = 3; n <= N; ++n)
+    {
+        dp[n] = dp[n - 2] + dp[n - 1];
+        dp[n] %= 10007;
+    }
+    
+    cout << dp[N] << '\n';
+    
+    return 0;
+}
